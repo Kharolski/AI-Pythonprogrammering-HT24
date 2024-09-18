7f7c4118b4b25d02b64c3a54bba5da451df68a8b
Kharolski <kharolski@gmail.com>
2024-09-18 22:49:05 +0200

    Adding gitignore file

--- Exercises/week1/uppgift4.py
+++
@@ -1,3 +0,0 @@
-# Skriv ett program som tar två strängar som input och skapar EN ny sträng där de två första tecken i varje sträng
-# bytts ut. Exempel-input: "abc", "xyz" Förväntad output: "xyc abz"
-

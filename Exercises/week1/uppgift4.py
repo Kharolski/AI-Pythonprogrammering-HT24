6a4bd444c6a11f5b2a4c0a90871d5f2d391bb3d1
Kharolski <kharolski@gmail.com>
2024-09-23 20:00:32 +0200

    initial commit

--- Exercises/week1/uppgift4.py
+++ Exercises/week1/uppgift4.py
@@ -18,3 +18,22 @@ input_text2 = input("Ange andra strängen: ")
 
 resultat = change_string(input_text1, input_text2)
 print(resultat)
+
+
+# Annat lösning
+ 
+# Vi konverterar strängerna till listor för att lättare kunna modifiera dem
+first_string = list(input("Enter the first string: "))
+second_string = list(input("Enter the second string: "))
+
+temp_character = first_string[0]    # Sparar ner första strängens första tecken i en "temporär" variabel
+first_string[0] = second_string[0]  # Byter värdet på första strängens första tecken
+second_string[0] = temp_character   # Byter värdet på andra strängens första tecken
+
+temp_character = first_string[1]    # Upprepar processen för strängarnas andra (second) tecken
+first_string[1] = second_string[1]
+second_string[1] = temp_character
+
+# Vi gör listorna till strängar igen med jon(), och lägger ihop till en enda sträng som sparas ner i variabeln 'combined_string'
+combined_string = "".join(first_string) + " " + "".join(second_string)  
+print(combined_string) 
--- Exercises/week1/uppgift7.py
+++ Exercises/week1/uppgift7.py
@@ -24,3 +24,17 @@ input_text = input("Skriv en komma-separerad sekvens av ord: ")
 
 result = sort_unique_words(input_text)
 print(result)
+
+
+# Annan lösning
+
+input_string_sequence = input("Enter a sequence of comma-separated strings: ")
+
+# Vi tar bort eventuell whitespace med replace, och delar sedan upp alla ord i strängen med split,
+# och stoppar in dem i en lista. sequence_list blir alltså en lista med orden som fanns i strängen. 
+sequence_list = input_string_sequence.replace(' ', '').split(",") # Vi splittar efter varje "," i strängen, och får på så vis strängen uppdelar ord-vis
+
+# Vi konverterar listan till ett set (en "mängd"), vilket automatiskt tar bort dubletter av värden i listan
+sequence_set = set(sequence_list)
+
+print(sorted(sequence_set)) # Får alfabetisk ordning på elementen i setet genom att använda sorted()

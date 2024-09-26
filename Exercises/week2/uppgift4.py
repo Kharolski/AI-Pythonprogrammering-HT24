53018a440f4be4ffd2d3c934062baf73e2d9b63c
Kharolski <kharolski@gmail.com>
2024-09-27 00:44:55 +0200

    commit

---
+++ Exercises/week2/uppgift4.py
@@ -0,0 +1,38 @@
+# Uppgift 4: Filläsning och ordräkning
+
+# Skapa en textfil (i samma mapp som ditt program). 
+# Skriv in eller kopiera in en text av valfri längd. 
+# Läs in textfilen och använd ett dictionary för att räkna förekomsten av varje ord. 
+# Ignorera skiljetecken och konvertera alla ord till lowercase.
+
+
+class FileWordCounter:
+    def __init__(self, filename):
+        self.filename = filename
+
+    # Rensa texten från skiljetecken och konvertera allt till små bokstäver.
+    def clean_text():
+        pass
+    
+    # Läs in textfilen och räkna förekomsten av varje ord.
+    def count_words():
+        pass
+
+    # Skriv ut ordlistan och antalet förekomster för varje ord.
+    def display_word_count():
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
